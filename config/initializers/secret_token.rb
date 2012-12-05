dbda1cc7a159cb22c4f2473c3450f12fc227f454
German Khokhlov <german@cma.ru>
2012-12-05 14:39:39 +0400

    Generated the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sourcing::Application.config.secret_token = 'aa6d7440841621a68297da5b74b19dfafef420695f39742aeada9dc07caf62fb8a0318643f42e21a6bb4fd3274a0e44dd637d415e3c9b22e1f7bcc5f777aae10'
