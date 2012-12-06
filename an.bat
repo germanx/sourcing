36b5393afbc4b28bcb2c471478ef73a0e33a4fbe
German Khokhlov <german@cma.ru>
2012-12-06 15:45:53 +0400

    Creation of projects feature complete

---
+++ an.bat
@@ -0,0 +1,1 @@
+c:\ansicon\ansicon -i
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Sourcing::Application.routes.draw do
+
+  root :to => "projects#index"
+
+  resources :projects
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
