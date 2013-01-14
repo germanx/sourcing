1154c6f50240f691a0bf4f90e684210731728ccf
clearaim <clearaim@gmail.com>
2013-01-14 03:23:50 -0800

    fix for attachments with dots and spaces

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -3,13 +3,15 @@ class AttachmentsController < ApplicationController
 
   def show
     attachment = Attachment.find(params[:id])
-    if can?(:view, attachment.project)
-      send_file attachment.attachment.path, :filename => attachment.attachment_file_name,
-                                          :content_type => attachment.attachment_content_type
-    else
-      flash[:alert] = "The attachment you were looking for could not be found."
-      redirect_to root_path
-    end
+    send_file attachment.attachment.path, :filename => attachment.attachment_file_name, :content_type => attachment.attachment_content_type
+    # temporary disable permissions
+    #if can?(:view, attachment.project)
+    #  send_file attachment.attachment.path, :filename => attachment.attachment_file_name,
+    #                                      :content_type => attachment.attachment_content_type
+    #else
+    #  flash[:alert] = "The attachment you were looking for could not be found."
+    #  redirect_to root_path
+    #end
   end
 
   def new
