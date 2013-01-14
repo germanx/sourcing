0df3b17bf3773a28c8a59891b4cdbe589ba6130c
clearaim <clearaim@gmail.com>
2013-01-14 03:57:53 -0800

    permissions added to attachment, admin now can manage all (in cancan terms)

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -3,15 +3,12 @@ class AttachmentsController < ApplicationController
 
   def show
     attachment = Attachment.find(params[:id])
-    send_file attachment.attachment.path, :filename => attachment.attachment_file_name, :content_type => attachment.attachment_content_type
-    # temporary disable permissions
-    #if can?(:view, attachment.project)
-    #  send_file attachment.attachment.path, :filename => attachment.attachment_file_name,
-    #                                      :content_type => attachment.attachment_content_type
-    #else
-    #  flash[:alert] = "The attachment you were looking for could not be found."
-    #  redirect_to root_path
-    #end
+    if can?(:read, attachment.project)
+      send_file attachment.attachment.path
+    else
+      flash[:alert] = "The attachment you were looking for could not be found (permission denied)."
+      redirect_to root_path
+    end
   end
 
   def new
