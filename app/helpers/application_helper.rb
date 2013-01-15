661898edbcad802c076dc1e14e27d4ee4d687fa2
German Khokhlov <german@cma.ru>
2013-01-15 18:50:01 +0400

    Publisher

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,11 @@ module ApplicationHelper
     nil
   end  
 
+  def publishers_only(&block)
+    block.call if current_user.try(:publisher?)
+    nil
+  end  
+
   def authorized?(permission, thing, &block)
     block.call if can?(permission.to_sym, thing) ||
         current_user.try(:admin?)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130111145534) do
+ActiveRecord::Schema.define(:version => 20130115115139) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -31,17 +31,6 @@ ActiveRecord::Schema.define(:version => 20130111145534) do
     t.integer  "project_id"
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
-  end
-
-  create_table "bidders", :force => true do |t|
-    t.string   "company"
-    t.string   "name"
-    t.string   "email"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.datetime "created_at",           :null => false
-    t.datetime "updated_at",           :null => false
   end
 
   create_table "comments", :force => true do |t|
@@ -154,6 +143,7 @@ ActiveRecord::Schema.define(:version => 20130111145534) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.boolean  "admin",                                 :default => false
+    t.boolean  "publisher",                             :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
