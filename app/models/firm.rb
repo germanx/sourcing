1b49bd9a48244a0d7fe11e1195c27cf57196cb71
Ubuntu <user@ubuntu.(none)>
2013-01-22 06:26:24 -0800

    seed

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -2,7 +2,7 @@ class Firm < ActiveRecord::Base
   attr_accessible :name, :description, :email, :id
 
   validates :name,        presence: true, length: { maximum: 100 }
-  validates :description, presence: true, length: { maximum: 250 }
+  validates :description, length: { maximum: 250 }
   validates :email,       length: { maximum: 50 }
 
   belongs_to :firm
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,68 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.delete_all
-admin_user = User.create(:email => 'german@cma.ru', 
-                         :password => "123456")
-admin_user.admin = true
-admin_user.save
-
-Project.delete_all
-prj = Project.create(:name => 'Ticketee', :description => 'Ticketee Descr', :number => 'P1')
-prj.user = admin_user
-prj.save
-
+# States
 State.delete_all
-State.create(:name => 'New',
+state_new = State.create(:name => 'New',
              :background => '#85FF00',
              :color => 'white')
-State.create(:name => 'Open',
+State.create(:name => 'Published',
              :background => '#00CFFD',
              :color => 'white')
 State.create(:name => 'Closed',
              :background => 'black',
              :color => 'white')
+State.create(:name => 'Done',
+             :background => 'black',
+             :color => 'white')
+
+# Firms & Users
+Firm.delete_all
+User.delete_all
+
+# Admin
+admin_firm = Firm.create(:email => 'info@admin.ru', :name => "Admin")
+admin_firm.save
+
+admin_user = User.create(:email => 'info@admin.ru', 
+                         :password => "123456")
+admin_user.admin = true
+admin_user.firm = admin_firm
+admin_user.save
+
+# Publisher
+publisher_firm = Firm.create(:email => 'info@publisher.ru', :name => "Publisher")
+publisher_firm.save
+
+publisher_user = User.create(:email => 'info@publisher.ru', 
+                   :password => "123456")
+publisher_user.publisher = true
+publisher_user.firm = publisher_firm
+publisher_user.save
+
+# Vendor
+vendor_firm = Firm.create(:email => 'info@vendor.ru', :name => "Vendor")
+vendor_firm.firm = publisher_firm
+vendor_firm.save
+
+vendor_user = User.create(:email => 'info@vendor.ru', 
+                         :password => "123456")
+vendor_user.firm = vendor_firm
+vendor_user.save
+
+# Project
+Project.delete_all
+Permission.delete_all
+
+prj = Project.create(:name => 'Portal', :description => 'Portal Descr', :number => 'P1')
+prj.user = publisher_user
+prj.firm = publisher_firm
+prj.state = state_new
+prj.save
+
+Permission.create!(:user => publisher_user,
+                         :thing => prj,
+                         :action => 'view')
+Permission.create!(:user => publisher_user,
+                         :thing => prj,
+                         :action => 'edit')
